80bd67ea216dd73f7f47db8021540c58d55a840e
gongpd <gpd19921125@163.com>
2020-09-16 20:55:45 +0800

    header组件静态

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -41,7 +41,7 @@ export default class Admin extends Component {
         </Sider>
         <Layout>
           <Header>Header</Header>
-          <Content>
+          <Content style={{margin:20,backgroundColor:'#fff'}}>
             <Switch>
               
               <Route path='/home' component={Home} />
